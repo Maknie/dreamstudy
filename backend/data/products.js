72e004586cb0b01165a0e8cc13e19d60d828c2ae
maknie <maratov.maknie@gmail.com>
2021-01-25 19:06:34 +0600

    new data

--- backend/data/products.js
+++ backend/data/products.js
@@ -24,53 +24,30 @@ const products = [
     numReviews: 0,
   },
   {
-    name: 'Bed "Stella"',
-    image: '/images/bed.jpg',
+    name: 'Language Courses London',
+    image: '/images/NewYork.jpg',
     description:
-      'Length: 2120 mm, Width: 1360 mm, Height: 800 mm, Material: frame – particleboard, upholstery – leatherette',
-    brand: 'Stella',
-    category: 'bed',
-    price: 215,
-    countInStock: 15,
+      'Cactus Language offers more than 20 different languages in 5 city-centre language schools across London. We have been running evening language courses in London for more than 20 years and have been featured on BBC National News, SkyOne, The Guardian and the Sunday Times, among others.',
+    brand: 'London',
+    category: 'language course',
+    price: 285,
+    countInStock: 99,
     rating: 0,
     numReviews: 0,
   },
   {
-    name: 'Bed "Felicita"',
-    image: '/images/bed1.jpg',
+    name: 'Language Courses London',
+    image: '/images/NewYork.jpg',
     description:
-      'Length: 2120 mm, Width: 1360 mm, Height: 800 mm, Material: frame – particleboard, upholstery – leatherette',
-    brand: 'Felicita',
-    category: 'bed',
-    price: 298,
-    countInStock: 0,
+      'Cactus Language offers more than 20 different languages in 5 city-centre language schools across London. We have been running evening language courses in London for more than 20 years and have been featured on BBC National News, SkyOne, The Guardian and the Sunday Times, among others.',
+    brand: 'London',
+    category: 'language course',
+    price: 285,
+    countInStock: 99,
     rating: 0,
     numReviews: 0,
   },
-  {
-    name: 'Table "Capella"',
-    image: '/images/table.jpg',
-    description:
-      'Height: 500 mm, d: 1000 mm, Material: frame – colored metal; plywood, MDF',
-    brand: 'Capella',
-    category: 'table',
-    price: 49.55,
-    countInStock: 10,
-    rating: 0,
-    numReviews: 0,
-  },
-  {
-    name: 'M Cherie',
-    image: '/images/chair.jpg',
-    description:
-      'Height:  1040-1140  mm, Width: 510 mm, Depth: 480 mm, Back height: 610 mm, Height from floor to seat: 500-600 mm',
-    brand: 'Cherie',
-    category: 'chair',
-    price: 81.02,
-    countInStock: 7,
-    rating: 0,
-    numReviews: 0,
-  },
+  
 ]
 
 export default products
